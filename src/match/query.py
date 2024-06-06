8eabaa85dbc3ccd0241eab902bbed175c058d25a
Arun Shankar <arunprshankar@gmail.com>
2024-06-06 11:30:28 -0400

    index creation and match modules done

--- src/match/query.py
+++ src/match/query.py
@@ -7,22 +7,20 @@ from typing import Dict
 from typing import Any 
 
 
-NUM_NEIGHBOURS = 3  # Retrieve the top matching neighbor (query)
-DEPLOYED_INDEX_NAME = 'queries_2024_06_06_09_34_29'
-INDEX_ENDPOINT_ID = '3228997369940934656'
+NUM_NEIGHBOURS = 1  # Retrieve the top matching neighbor (query)
+DEPLOYED_INDEX_NAME = 'queries_2024_06_06_10_49_50'
+INDEX_ENDPOINT_ID = '5044159126003777536'
 
 def retrieve_top_match(api_response: List[Dict[str, Any]]) :
-   
-    o = []
-
+    top_match = {}
 
     try:
         for match in api_response[0]:  # Access the first (and possibly only) list of matches
-            print(match)
+            top_match['confidence'] = round(1 - match.distance, 4)
             for restrict in match.restricts:
-                if restrict.name == 'page_content':
+                if restrict.name == 'question':
                     # Assuming that the desired content is always the first token in 'allow_tokens'
-                    o.append(restrict.allow_tokens[0])
+                    top_match['query'] = restrict.allow_tokens[0]
     except IndexError as e:
         logger.error(f"Error accessing elements in the API response: Index out of range - {e}")
     except KeyError as e:
@@ -30,12 +28,11 @@ def retrieve_top_match(api_response: List[Dict[str, Any]]) :
     except Exception as e:
         logger.error(f"An unexpected error occurred - {e}")
 
-    return o
+    return top_match
 
 if __name__ == "__main__":
     query = "Microsoft 365?"
     query_embedding = get_query_embedding(query)
-    print(query_embedding)
     response = find_neighbors(query_embedding, DEPLOYED_INDEX_NAME, INDEX_ENDPOINT_ID, NUM_NEIGHBOURS)
     top_match = retrieve_top_match(response)
     print(top_match)
