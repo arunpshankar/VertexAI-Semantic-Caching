6d1d79df140345c6babbe06903d9eed6380b1aca
Arun Shankar <arunprshankar@gmail.com>
2024-06-06 08:24:17 -0400

    wip

--- src/match/doc.py
+++ src/match/doc.py
@@ -4,11 +4,13 @@ from vertexai.preview.language_models import TextEmbeddingModel
 from src.config.logging import logger
 from src.config.setup import config
 from typing import List
+from typing import Dict
+from typing import Any 
 
 
 NUM_NEIGHBOURS = 3  # Retrieve the top matching pages
-DEPLOYED_INDEX_NAME = 'earnings_report_2024_06_04_13_27_05'
-INDEX_ENDPOINT_ID = '4519278663182581760'
+DEPLOYED_INDEX_NAME = 'earnings_report_2024_06_06_07_11_43'
+INDEX_ENDPOINT_ID = '2462259533381107712'
 
 def get_query_embedding(query: str) -> List[float]:
     """Generates embeddings for a given query.
@@ -47,7 +49,38 @@ def find_neighbors(query_embedding: List[float]):
         logger.info('-' * 30)
 
 
+def extract_page_content(api_response: List[Dict[str, Any]]) -> List[str]:
+    """
+    Extracts page contents from an API response.
+
+    This function parses a structured API response to extract page contents specified under the 'page_content' key.
+    It assumes the API response is a list of dictionaries containing lists of 'restricts' dictionaries.
+
+    Parameters:
+    api_response (List[To handle a structured API response where each item is a dictionary containing further information.
+
+    Returns:
+    List[str]: A list of strings, each representing the page content extracted from the API response.
+    """
+    page_contents = []
+    try:
+        for match in api_response[0]:  # Access the first (and possibly only) list of matches
+            for restrict in match.restricts:
+                if restrict.name == 'page_content':
+                    # Assuming that the desired content is always the first token in 'allow_tokens'
+                    page_contents.append(restrict.allow_tokens[0])
+    except IndexError as e:
+        logger.error("Error accessing elements in the API response: Index out of range", exc_info=True)
+    except KeyError as e:
+        logger.error("Key error in accessing API response data; possibly malformed response", exc_info=True)
+    except Exception as e:
+        logger.error("An unexpected error occurred", exc_info=True)
+    return page_contents
+
 if __name__ == "__main__":
     query = "How many Microsoft 365 Consumer subscribers were there as of Q2 2021?"
     query_embedding = get_query_embedding(query)
-    find_neighbors(query_embedding)
+    response = find_neighbors(query_embedding)
+    pages = extract_page_content(response)
+    for page in pages:
+        print(page[:100])
