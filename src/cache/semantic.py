5d944434d78f41dde3c3267ead091ab4260b6e1b
Arun Shankar <arunprshankar@gmail.com>
2024-06-06 16:02:03 -0400

    wip

--- src/cache/semantic.py
+++ src/cache/semantic.py
@@ -1,26 +1,27 @@
 from src.match.doc import retrieve_top_matches
 from src.match.query import retrieve_top_match
 from src.config.logging import logger
+from src.generate.qa import generate_answer
 
 
 def query_match(question: str):
     match = retrieve_top_match(question)
-    print(match)
+    return match
 
 
-def doc_match():
+def doc_match(question: str):
     matches = retrieve_top_matches(question)
     context = '\n\n'.join(matches)
-       
-
-
+    answer = generate_answer(question, context)
+    return answer
 
 
 def add():
     pass
 
 
-
 if __name__ == '__main__':
     question = "What was the operating income or loss (in billions) for Google Cloud for Q1 of 2021 compared to the previous year?"
-    answer = query_match(question)
+    match = query_match(question)
+    answer = doc_match(question)
+    print(answer)
